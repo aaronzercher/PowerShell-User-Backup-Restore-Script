c83e2926496dae50e9a2778c43b7bd4edff3f512
Aaron Zerchr <42446385+aaronzercher@users.noreply.github.com>
2020-07-28 14:42:11 -0400

    Update UserDataBackup.ps1
    I made a lot of changes to the file.

--- UserDataBackup.ps1
+++ UserDataBackup.ps1
@@ -1,72 +1,114 @@
 ##
 ######## Powershell Script for Backing up User Data ########
 ##
-######## v1.0 11/23/18 ########
+######## Also Deletes Browser Cache ########
+##
+######## v5.0 7/20/20 ########
 ##
 ######## By Aaron Zercher ########
 ##
-######## Script will backup all data in Desktop, Documents, Downloads, Favorites, Music, Pictures, Videos, Chrome, and Mozilla Data ########
+######## Script will backup all data in Desktop, Documents, Downloads, Favorites, Pictures, Chrome, and Mozilla Data ########
 ##
-######## Declares the values and prompts for Technician, Username and Password information ########
-$Technician = Read-Host -Prompt "What Technician is auditing this computer (First name only)"
-$Username = Read-Host -Prompt "Who does this computer belong to (First initial, last name)"
-$NetworkPassword = Read-Host -Prompt "Please enter the network password"
+##region CacheFolderDeletion Variables (Currently supports Chrome and Firefox)
+$delete = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Cache\",
+  "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Code Cache\",
+  "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Service Worker\ScriptCache",
+  "$env:LOCALAPPDATA\Google\Chrome\User Data\Profile 1\Cache\",
+  "$env:LOCALAPPDATA\Google\Chrome\User Data\Profile 1\Code Cache\",
+  "$env:LOCALAPPDATA\Google\Chrome\User Data\Profile 1\Service Worker\ScriptCache",
+  "$env:LOCALAPPDATA\Mozilla\Firefox\Profiles\*.default\cache",
+  "$env:LOCALAPPDATA\Mozilla\Firefox\Profiles\*.default\cache2"
+##endregion CacheFolderDeletion Variables
 
-######## Declares the Backup location ########
-$destination = "<#Desired Location here#>"
+##region DeleteFiles
+Write-Host -ForegroundColor Cyan "Begin Browser Cleanup"
+Get-ChildItem -Path $delete | Remove-Item -Verbose -Recurse
+##endregion
 
-######## Declares the data to be backed up ########
+##region Switch to Backup
+Start-Sleep -Seconds 30
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host -ForegroundColor Yellow "Standby Loading Backup Script..."
+Start-Sleep -Seconds 10
+Clear-Host
+##end region Switch to Backup
+
+#region GetVariables
+[String]$Technician = Read-Host -Prompt "What Technician is auditing this computer (First name only)"
+[String]$NetworkPassword = Read-Host -Prompt "Please enter the Users network password" ##NEEDED
+#endregion GetVariables
+
+#region DeclaringBackuplocation
+$destination = "(Your Destination here)"
+#endregion DeclaringBackuplocation
+
+#region DeclaringDataBackupSources
 $folder = "Desktop",
-"Downloads",
-"Favorites",
-"Documents",
-"Music",
-"Pictures",
-"Videos",
-"AppData\Local\Mozilla",
-"AppData\Local\Google\Chrome",
-"AppData\Roaming\Mozilla"
-
-######## Calls Eviroment Variables for the local user and data location ########
-$username = gc env:username
-$userprofile = gc env:userprofile
-$appData = gc env:localAPPDATA
+  "Downloads",
+  "Favorites",
+  "Documents",
+  "Pictures",
+  "AppData\Local\Google\Chrome\User Data",
+  "AppData\Local\Mozilla\Firefox\Profiles",
+  "AppData\Roaming\Mozilla\Firefox"
+#endregion DeclaringDataBackupSources
 
 ###### Backup Data section ########
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host -ForegroundColor green "Backing up data from local machine for $env:USERNAME"
 
-	write-host -ForegroundColor green "Backing up data from local machine for $username"
+#region Folder creation
+New-Item -Type Directory -Path $destination -Name "Audit Information" -Force | Out-Null
+New-Item -Type Directory -Path $destination -Name "RegistryInformation" -Force | Out-Null
+New-Item -Type Directory -Path $destination -Name "RegistryInformation\Temp" -Force | Out-Null
+#endregion Folder creation
 
-    New-Item -Type Directory -Path ($destination + "\Audit Information") -Force |Out-Null
-    New-Item -Type Directory -Path ($destination + "\RegistryInformation") -Force |Out-Null
+#region DataBackup
+ForEach ($f in $folder) {
+    $currentLocalFolder = Join-Path -Path $env:USERPROFILE -ChildPath $f
+    $currentRemoteFolder = Join-Path -Path $destination -ChildPath $f
+    $currentFolderSize = (Get-ChildItem -ErrorAction silentlyContinue $currentLocalFolder -Recurse -Force | Measure-Object -ErrorAction silentlyContinue -Property Length -Sum ).Sum / 1MB
+    $currentFolderSizeRounded = [System.Math]::Round($currentFolderSize)
+    Write-Host -ForegroundColor cyan "  $f... ($currentFolderSizeRounded MB)"
+    Copy-Item -ErrorAction silentlyContinue -recurse $currentLocalFolder $currentRemoteFolder
+}
+#endregion DataBackup
 
-    foreach ($f in $folder)
-	{
-		$currentLocalFolder = $userprofile + "\" + $f
-		$currentRemoteFolder = $destination + "\" + $f
-		$currentFolderSize = (Get-ChildItem -ErrorAction silentlyContinue $currentLocalFolder -Recurse -Force | Measure-Object -ErrorAction silentlyContinue -Property Length -Sum ).Sum / 1MB
-		$currentFolderSizeRounded = [System.Math]::Round($currentFolderSize)
-		write-host -ForegroundColor cyan "  $f... ($currentFolderSizeRounded MB)"
-		Copy-Item -ErrorAction silentlyContinue -recurse $currentLocalFolder $currentRemoteFolder
-	}
+#Yes this is unsecure but needs to be done.
+##region PasswordFileCreation
+Out-file -filepath "$destination\Audit Information\NewBuild.txt" -inputobject $NetworkPassword
+##endregion PasswordFileCreation
 
-######## Writes the password to a .txt file in Plain Text ########
-    Out-file -filepath "$destination\Audit Information\NewBuild.txt" -inputobject $NetworkPassword
+##region Registry Backup
+Write-Host -ForegroundColor green "Backing up registry data from local machine for $env:USERNAME"
+  $keys = "HKCU\Network", "HKCU\Printers", "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Devices", "HKCU\Software\Microsoft\Windows NT\CurrentVersion\PrinterPorts", "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows"
 
-######## Begin Registry Backup ########
-	Write-Host -ForegroundColor Green "Backing up Network settings"
-	Get-ItemProperty -Path "HKCU:\NETWORK" | Export-CliXML "$destination\RegistryInformation\network.reg"
-	Write-Host -ForegroundColor Green "Network Settings Backup Completed Successfully"
-	Write-Host -ForegroundColor Green "Backing up Printers"
-	Get-ItemProperty -Path "HKCU:\Printers" | Export-CliXML "$destination\RegistryInformation\printers.reg"
-	Write-Host -ForegroundColor Green "Printers Backup Completed Successfully"
-	Write-Host -ForegroundColor Green "Backing up Printers1"
-	Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Devices" | Export-Clixml "$destination\RegistryInformation\printers1.reg"
-	Write-Host -ForegroundColor Green "Printers 1 Backup Completed Successfully"
-	Write-Host -ForegroundColor Green "Backing up Printer2"
-	Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\PrinterPorts" | Export-Clixml "$destination\RegistryInformation\printers2.reg"
-	Write-Host -ForegroundColor Green "Printers 2 Backup Completed Successfully"
-	Write-Host -ForegroundColor Green "Backing up Printers3"
-	Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows" | Export-Clixml "$destination\RegistryInformation\printers3.reg"
-	Write-Host -ForegroundColor Green "Printers 3 Backup Completed Successfully"
+  $tempFolder = "$destination\RegistryInformation\temp"
+  $outputFile = "$destination\RegistryInformation\RegBackup.reg"
 
-	write-host -ForegroundColor green "Backup complete!"
+$keys | ForEach-Object {
+  $i++
+  & reg export $_ "$tempFolder\$i.reg"
+}
+
+"Windows Registry Editor Version 5.00" | Set-Content $outputFile
+Get-Content "$tempFolder\*.reg" | Where-Object {
+  $_ -ne "Windows Registry Editor Version 5.00"
+} | Add-Content $outputFile
+##endregion Registry Backup
+Write-Host ""
+Write-Host -ForegroundColor green "Registry Backup Complete"
+Write-Host ""
+Write-Host ""
+Write-Host -ForegroundColor green "Backup complete! You my shutdown the computer"
+Start-Sleep -Seconds 15
+
+##region shutdownPC
+Stop-Computer -Confirm
+##endregion shutdownPC
+
+##Script completes
